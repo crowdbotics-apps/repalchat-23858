a368763f5b49afcd09ed6762201a6232b291a350
Crowdbotics <team@crowdbotics.com>
2021-01-15 08:44:58 +0000

    Update models

--- backend/repalchat_23858/wsgi.py
+++ backend/repalchat_23858/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repalchat_23858.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "repalchat_23858.settings")
 
 application = get_wsgi_application()
